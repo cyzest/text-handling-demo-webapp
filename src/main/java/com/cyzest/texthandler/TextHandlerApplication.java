a82059452787d610ab5fa05b03ceac877a341371
cyzest <cyzest@nate.com>
2018-10-30 21:24:19 +0900

    Spring Boot 설정

---
+++ src/main/java/com/cyzest/texthandler/TextHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.cyzest.texthandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextHandlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextHandlerApplication.class, args);
+    }
+
+}
